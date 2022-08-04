c403a9492722a579f0cac3de71ae4eb6e9814246
Rich Hall <8127551+Rich-Hall@users.noreply.github.com>
2022-08-04 14:54:42 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='Sentinel1Decoder'
+  url='https://github.com/Rich-Hall/sentinel1decoder'
+  author='Rich Hall'
+  packages='sentinel1decoder'
+  install_requires=['numpy', 'pandas']
+  version='0.1'
+  license='GPL-3.0'
+  description='A python decoder for ESA Sentinel-1 Level0 files'
+)
