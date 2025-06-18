e1c0ce47e1fb978895f73fa96cdd228534f1a93e
Rich Hall <richardhall434@gmail.com>
2025-06-17 21:06:52 -0600

    Another Rust fix

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,15 @@
 use pyo3::prelude::*;
 
+/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> String {
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+/// A Python module implemented in Rust.
 #[pymodule]
-fn sentinel1decoder(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)
+fn string_sum(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+
+    Ok(())
 }
