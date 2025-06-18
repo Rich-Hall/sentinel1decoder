e26ab2dac55d4ac93bf62d25868c83ca38b70328
Rich Hall <richardhall434@gmail.com>
2025-06-17 21:08:00 -0600

    Asfojghafga

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,8 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn string_sum(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn sentinel1decoder(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-
     Ok(())
 }
