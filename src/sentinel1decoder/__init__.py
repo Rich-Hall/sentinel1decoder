083667d370f9b6ea3487e3d745011e998a57052b
Rich Hall <8127551+Rich-Hall@users.noreply.github.com>
2026-01-03 16:45:26 -0700

    Fixed version number mismatch in __init__ and pyproject.toml (#36)

--- src/sentinel1decoder/__init__.py
+++ src/sentinel1decoder/__init__.py
@@ -4,7 +4,7 @@ from . import constants, utilities
 from .l0decoder import Level0Decoder
 from .l0file import Level0File
 
-__version__ = "0.1"
+__version__ = "1.1.1"
 
 __all__ = [
     "Level0Decoder",
